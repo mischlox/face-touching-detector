d8623d770634bf5c1cec975c03bbd5c0b3d10df6
mischlox <michael-schlosser99@gmx.de>
2022-09-11 13:56:08 +0200

    Add GPU toggle to detectors factory

--- src/ft-detector/include/ft-detector/detectors.h
+++ src/ft-detector/include/ft-detector/detectors.h
@@ -8,8 +8,8 @@
 class Detectors {
    public:
     static std::unique_ptr<Detector> yolov5(const std::string &modelPath,
-                                            const std::string &classListPath) {
-        return std::make_unique<YoloV5>(modelPath, classListPath);
+                                            const std::string &classListPath, bool useGPU = true) {
+        return std::make_unique<YoloV5>(modelPath, classListPath, useGPU);
     }
 };
 
